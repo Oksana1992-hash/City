e026904c2df0472ec2ce11bc247e3302b70f280e
Докс <nox23061992@gmail.com>
2024-10-25 22:15:54 +0300

    Возвращаем результаты на русском

--- Coord_citi_main.py
+++ Coord_citi_main.py
@@ -4,9 +4,7 @@ from opencage.geocoder import OpenCageGeocode
 def get_coordinates(city, key):
     try:
         geocoder = OpenCageGeocode(key)
-        query = city # запрос
-        results = geocoder.geocode(query) # результат, который мы получим, отправив запрос
-
+        results = geocoder.geocode(city, language='ru') # результат, который мы получим, отправив запрос
         if results: # Если результат поиска не пустой:
             return results[0]['geometry']['lat'], results[0]['geometry']['lng']
         else:
